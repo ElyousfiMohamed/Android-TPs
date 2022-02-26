ce3246dd19abf8f52366ac9a3468996a33cb3960
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-02-26 12:32:23 +0100

    Verification + multiple calc

--- Calculatrice/app/src/main/java/com/example/calculatrice/MainActivity.java
+++ Calculatrice/app/src/main/java/com/example/calculatrice/MainActivity.java
@@ -5,28 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Process;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText ec;
-    int number;
-    int temp;
+    ListView lv;
+    List<String> operations;
+    ArrayAdapter aad;
+    int temp,temp1,temp2;
     char op;
     boolean x = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = findViewById(R.id.lv);
+        operations = new ArrayList<>();
+        aad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,operations);
+        lv.setAdapter(aad);
     }
 
     public void calc(View view) {
         Button btn = (Button) view;
         ec = findViewById(R.id.ecran);
         ec.setText(ec.getText().toString()+btn.getText());
-        number = Integer.parseInt(ec.getText().toString());
+        temp = Integer.parseInt(ec.getText().toString());
         x = true;
     }
 
@@ -35,38 +48,45 @@ public class MainActivity extends AppCompatActivity {
         ec = findViewById(R.id.ecran);
         op = btn.getText().charAt(0);
         ec.setText("");
-        temp = number;
+        temp1 = temp;
         x = false;
     }
 
     public void res(View view) {
             if(x == false) {
-                Toast.makeText(this, "The second number ?", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "le deuxieme nombre n'est pas saisie", Toast.LENGTH_SHORT).show();
                 return ;
             }
+            temp2 = temp;
             ec.setText("");
             switch (op) {
                 case '+' :
-                    int res= temp+number;
-                    System.out.println(res);
-                    ec.setText(Integer.toString(res));
+                    temp = temp1+temp2;
+                    ec.setText(Integer.toString(temp));
                     break;
                 case '-' :
-                    res= temp-number;
-                    System.out.println(res);
-                    ec.setText(Integer.toString(res));
+                    temp = temp1-temp2;
+                    ec.setText(Integer.toString(temp));
                     break;
                 case '*' :
-                    res= temp*number;
-                    System.out.println(res);
-                    ec.setText(Integer.toString(res));
+                    temp = temp1*temp2;
+                    ec.setText(Integer.toString(temp));
                     break;
                 case '/' :
-                    res= temp/number;
-                    System.out.println(res);
-                    ec.setText(Integer.toString(res));
+                    temp = temp1/temp2;
+                    ec.setText(Integer.toString(temp));
                     break;
             }
+    }
+
+    public void add (View view){
+        operations.add(temp1 + " " + op + " "+temp2+" = " +  temp);
+        aad.notifyDataSetChanged();
+    }
+
+    public void vider(View view) {
+        ec = findViewById(R.id.ecran);
+        ec.setText("");
     }
 
 
