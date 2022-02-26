1a7745d43bdfbeb0138b3f34c47975b5bfeac8cb
ELYOUSFI Mohamed <mohamed5elyousfi@gmail.com>
2022-02-26 12:58:19 +0100

    vider l'ecran

--- Calculatrice/app/src/main/java/com/example/calculatrice/MainActivity.java
+++ Calculatrice/app/src/main/java/com/example/calculatrice/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void vider(View view) {
         ec = findViewById(R.id.ecran);
+        temp = temp1 = temp2 = 0;
         ec.setText("");
     }
 
